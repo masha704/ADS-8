a6843b5a10dc626cca14eabdf6f79d06ed4bda3c
Maria Anikina <59134557+masha704@users.noreply.github.com>
2022-05-08 23:37:55 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -6,25 +6,37 @@ Train::Train() :countOp(0), first(nullptr) {}
 Train::Cage* Train::create(bool light) {
   Cage *cage = new Cage;
   cage->light = light;
-  cage->next = nullptr;
-  cage->prev = nullptr;
+  //cage->next = nullptr;
+  //cage->prev = nullptr;
   return cage;
 }
 
 void Train::addCage(bool light) {
-  if (first != nullptr) {
-    Cage *temp = create(light);
+  //if (first != nullptr) {
+    //Cage *temp = create(light);
     //temp->prev = first->prev;
     //temp->prev->next = temp;
     //first->prev = temp;
     //first->prev->next = first;
-    temp->next = first->next;
-    temp->prev = first;
-    first->next->prev = temp;
-    first->next = temp;
-  } else {
-    first = create(light);
+    //temp->next = first->next;
+    //temp->prev = first;
+    //first->next->prev = temp;
+    //first->next = temp;
+  //} else {
+    //first = create(light);
+  //}
+  Cage *temp = new Cage;
+  temp->light = light;
+  if(!first) {
+    temp->next = nullptr;
+    temp->prev = nullptr;
+    first = temp;
+    return;
   }
+  temp->next = first->next;
+  temp->prev = first;
+  first->next->prev = temp;
+  first->next = temp;
 }
 
 int Train::getLength() {
@@ -32,7 +44,6 @@ int Train::getLength() {
   first->light = true;
   int step = 0;
   countOp = 0;
-  int length = 0;
   while (true) {
     step++;
     countOp++;
